1bf6b9fd1eb268896941de6abd8f31464331a6b6
Ten One Design <peter@tenonedesign.com>
2022-11-18 23:55:52 -0500

    - added rpc switching - refactored wallets - improved pool layout - added pool wallet

--- src/lib/Provider.svelte
+++ src/lib/Provider.svelte
@@ -1,50 +1,43 @@
 <script lang="ts">
-	import { address, wallet, language } from '$lib/stores';
+	import { address, wallet, language, selectedRpc, customRpc } from '$lib/stores';
 	import * as kondor from "kondor-js";
 	import { Signer, Contract, Provider, Serializer, utils } from "koilib";
 	import {onMount, onDestroy} from 'svelte';
+	import { Balances, Wallet } from '$lib/types';
+	import { tokenBalance } from '$lib/utils';
+	import type { Writable } from 'svelte/store';
 
 	onMount(async () => {
 		if ($address) {
-			getBalance();
+			$wallet.getBalances($address, $selectedRpc || $customRpc).then(() => {
+				wallet.set($wallet);
+			});
 		}
 	});
-
-	const tokenBalance = async (contractId: string, address: string): Promise<number> => {
-		const contract = new Contract({
-			id: contractId,
-			abi: utils.tokenAbi,
-			provider: kondor.provider,
-			signer: kondor.signer,
-		});
-		const { result } = await contract.functions.balanceOf({ owner: address });
-		return Promise.resolve(result?.value as number || 0);
-	}
-	const getBalance = async () => {
-
-		const koinBalance = await tokenBalance("15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL", $address);
-		const vhpBalance = await tokenBalance("1AdzuXSpC6K9qtXdCBgD5NUpDNwHjMgrc9", $address);
-		const manaBalance = await kondor.provider.getAccountRc($address) || 0;
-		wallet.set({ balances: {koin: koinBalance, mana: manaBalance, vhp: vhpBalance}});
-	}
 
 	const connect = async () => {
 		// setIsConnecting(true);
 		// @ts-ignore getAccounts returns objects, not strings
-		const accounts = await Promise.race([
+		const accounts:[any] = await Promise.race([
 			kondor.getAccounts(),
 			new Promise((resolve) => setTimeout(() => resolve([{ address: "" }]), 10000))
 		]);
 
 		if (accounts[0].address) {
 			address.set(accounts[0].address);
-			getBalance();
+			$wallet.getBalances(accounts[0].address, $selectedRpc || $customRpc).then(() => {
+				wallet.set($wallet);
+			});
 		}
 		// if (address) setAccount(address);
 		// setIsConnecting(false);
 
 		return !!accounts[0].address;
 	};
+	const disconnect = async() => {
+		address.set("");
+		wallet.set(new Wallet());
+	}
 
 	
 </script>
@@ -52,6 +45,14 @@
 {#if $address == ""}
 	<button class="btn" on:click={connect}>Connect to Kondor</button>
 {:else}
-	<button class="btn" on:click={connect}>{$address.slice(0,5)+"..."+$address.slice(-5)}</button>
+	
+	<div class="dropdown dropdown-end ">
+		<button class="btn">{$address.slice(0,5)+"..."+$address.slice(-5)}</button>
+		<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+		<div tabindex="0" class="dropdown-content menu mt-2 p-2 shadow bg-base-100 rounded-box w-52">
+		  <li><button on:click={connect}>Open Kondor</button></li>
+		  <li><button on:click={disconnect}>Disconnect Kondor</button></li>
+		</div>
+	  </div>
 {/if}
 <style></style>
